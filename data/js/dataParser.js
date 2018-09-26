c4e79ee92d3ac4ea18b864ad2a6c4257fd707457
Ali Nadhum <ali.nadhum@rdvelho.com>
2018-09-26 23:15:44 +0300

    Added project to github

---
+++ data/js/dataParser.js
@@ -0,0 +1,134 @@
+$(document).ready(function () {
+  Highcharts.chart('container', {
+      chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+      },
+      title: {
+        text: 'Nykyinen lämpötila'
+      },
+      credits: {
+        enabled: false
+      },
+      pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+          backgroundColor: {
+            linearGradient: {
+              x1: 0,
+              y1: 0,
+              x2: 0,
+              y2: 1
+            },
+            stops: [
+              [0, '#FFF'],
+              [1, '#333']
+            ]
+          },
+          borderWidth: 0,
+          outerRadius: '109%'
+        }, {
+          backgroundColor: {
+            linearGradient: {
+              x1: 0,
+              y1: 0,
+              x2: 0,
+              y2: 1
+            },
+            stops: [
+              [0, '#333'],
+              [1, '#FFF']
+            ]
+          },
+          borderWidth: 1,
+          outerRadius: '107%'
+        }, {
+          // default background
+        }, {
+          backgroundColor: '#DDD',
+          borderWidth: 0,
+          outerRadius: '105%',
+          innerRadius: '103%'
+        }]
+      },
+
+      // the value axis
+      yAxis: {
+        min: 0,
+        max: 1024,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 1,
+        tickPosition: 'side',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+          step: 2,
+          rotation: 'auto'
+        },
+        title: {
+          text: ''
+        },
+        plotBands: [{
+            from: 0,
+            to: 100,
+            color: '#ffdd42'
+          }, {
+            from: 100,
+            to: 300,
+            color: '#FFA500'
+          }, {
+            from: 300,
+            to: 500,
+            color: '#FF8C00'
+          }, {
+            from: 500,
+            to: 800,
+            color: '#df7f00'
+          },
+          {
+            from: 800,
+            to: 1024,
+            color: '#FF4500'
+          }
+        ]
+      },
+
+      series: [{
+        name: 'Lämpotila',
+        data: [0],
+        tooltip: {
+          valueSuffix: '°C'
+        }
+      }]
+
+    },
+
+    // Add some life
+    function (chart) {
+      if (!chart.renderer.forExport) {
+        setInterval(function () {
+          var point = chart.series[0].points[0],
+            newVal,
+            inc = Math.round((Math.random() - 0.5) * 20);
+
+          newVal = point.y + inc;
+          if (newVal < 0 || newVal > 200) {
+            newVal = point.y - inc;
+          }
+          point.update(newVal);
+
+        }, 3000);
+      }
+    });
+});
---
+++ esp8266.ino
@@ -0,0 +1,125 @@
+/*
+ * ESP8266 SPIFFS HTML Web Page with JPEG, PNG Image 
+ *
+ */
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <FS.h>   //Include File System Headers
+#include <max6675.h>
+#include <ESP8266HTTPClient.h>
+
+ESP8266WebServer server(80);
+
+//ESP AP Mode configuration
+const char *ssid = "N300";
+const char *password = "12345678";
+
+const int thermoDO = 12;
+const int thermoCS = 13;
+const int thermoCLK = 14;
+
+MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+HTTPClient http;
+
+void handleRoot(){
+  server.sendHeader("Location", "/index.html", true);
+  server.send(302, "text/plain", " ");
+}
+
+void handleWebRequests(){
+  if(loadFromSpiffs(server.uri())) return;
+  String message = "File Not Detected\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)? "GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " NAME:"+server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  Serial.println(message);
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  Serial.println('\n');
+  
+  //Initialize File System
+  SPIFFS.begin();
+  Serial.println("File System Initialized");
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  Serial.println("Trying to connect to Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected..");
+  Serial.print("Use this URL to connect: http://");// Print the IP address
+  Serial.println(WiFi.localIP());
+  Serial.println("\r\n");
+  server.onNotFound(handleWebRequests); //Set setver all paths are not found so we can handle as per URI
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("Webserver started..."); // Start the webserver
+  Serial.print("\n");
+}
+
+void loop() {
+  server.handleClient();
+  if (WiFi.status() == WL_CONNECTED) {
+    float DC = thermocouple.readCelsius();
+    float DF = thermocouple.readFahrenheit();
+    if (isnan(DC) || isnan(DF)){
+      Serial.println("Failed to read temprature from esp-sensor");
+      return;
+    }
+    Serial.print("C = "); 
+    Serial.print(DC);
+    Serial.print("\n");
+    Serial.print(WiFi.localIP().toString());
+    http.begin("http://"+ WiFi.localIP().toString() +"/index.html");
+    
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        http.writeToStream(&Serial);
+      }
+      http.end();
+    }
+    delay(1000);
+  }
+}
+
+bool loadFromSpiffs(String path){
+  String dataType = "text/plain";
+  if(path.endsWith("/")) path += "index.htm";
+
+  if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
+  else if(path.endsWith(".html")) dataType = "text/html";
+  else if(path.endsWith(".htm")) dataType = "text/html";
+  else if(path.endsWith(".css")) dataType = "text/css";
+  else if(path.endsWith(".js")) dataType = "application/javascript";
+  else if(path.endsWith(".png")) dataType = "image/png";
+  else if(path.endsWith(".gif")) dataType = "image/gif";
+  else if(path.endsWith(".jpg")) dataType = "image/jpeg";
+  else if(path.endsWith(".ico")) dataType = "image/x-icon";
+  else if(path.endsWith(".xml")) dataType = "text/xml";
+  else if(path.endsWith(".pdf")) dataType = "application/pdf";
+  else if(path.endsWith(".zip")) dataType = "application/zip";
+  File dataFile = SPIFFS.open(path.c_str(), "r");
+  if (server.hasArg("download")) dataType = "application/octet-stream";
+  if (server.streamFile(dataFile, dataType) != dataFile.size()) {
+  }
+  dataFile.close();
+  return true;
+}
